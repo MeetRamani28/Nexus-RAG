7b5ab15f3109a94ab03ce3ae51e350e87088e0f3
Meet Ramani <meetramani21@gmail.com>
2026-07-25 17:16:20 +0530

    initialize Vite React-TypeScript project with Tailwind CSS setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline flex items-center justify-center">
+      Welcome To The Nexus-RAG Frontend
+    </div>
+  )
+}
+
+export default App
